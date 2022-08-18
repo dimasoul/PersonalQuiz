2def1ce5b19d3690b7fdef7c52557bfc6896f2da
mitya-toretto <dimahryasheff@yandex.ru>
2022-08-18 22:53:08 +0300

    ViewControllers add

---
+++ PersonalQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Дмитрий on 14.08.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
---
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Дмитрий on 18.08.2022.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
