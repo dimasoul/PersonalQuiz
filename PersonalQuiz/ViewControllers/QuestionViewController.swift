ec5fd58edc55962120b4c9dfb9c7c2af5b99f5c7
mitya-toretto <dimahryasheff@yandex.ru>
2022-08-20 16:39:36 +0300

    StoryBoard completed

--- PersonalQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 class QuestionViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabel: [UILabel]!
+    @IBOutlet var multipleSwitch: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabel: [UILabel]!
+    
+    @IBOutlet weak var rangedSlider: UISlider!
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    @IBAction func rangedAnsweButtonPressed() {
+    }
 }
